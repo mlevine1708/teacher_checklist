3721f927011b38394c48ae54029a6a29ae68adf8
Micah Levine <micah.levine@outlook.com>
2020-02-09 21:05:25 +0000

    successfull migration

--- db/migrate/20200209155323_create_assignments_table.rb
+++ db/migrate/20200209155323_create_assignments_table.rb
@@ -1,6 +1,6 @@
 class CreateAssignmentsTable < ActiveRecord::Migration[5.2]
   def change
-    create_table assignments do |t|
+    create_table :assignments do |t|
       t.string :title 
       t.string :subject 
       t.integer :user_id
