51b330cd7a2d27048d883340bb77cbe8366343e6
Micah Levine <micah.levine@outlook.com>
2020-02-10 18:49:19 +0000

    added form

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -5,6 +5,13 @@ class TeachersController < ApplicationController
   end
   #the purpose of this route is to render the login page/form 
   
+  
+  post '/login' do
+    
+    
+  end
+  #the purpose of this route is to receive the login form and sign the user in (create a session)
+  
   get '/signup' do
     
   end
