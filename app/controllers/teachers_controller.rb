da4fdb22573fe374ce35a4eb37215934feb4b9bf
Micah Levine <micah.levine@outlook.com>
2020-02-11 01:32:29 +0000

    added login details

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -7,7 +7,11 @@ class TeachersController < ApplicationController
   
   
   post '/login' do
-    
+    #Find the user
+    @teacher = Teacher.find_by(username: params[:username])
+    #Authenticate the user - verify the user is who they say they are 
+    #log the user in
+    #redirect to the user's landing page
     
   end
   #the purpose of this route is to receive the login form and sign the user in (create a session)
