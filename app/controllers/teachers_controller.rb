4297b6c4293149e23b3b9b6322912246a4639961
Micah Levine <micah.levine@outlook.com>
2020-02-11 02:43:44 +0000

    authenticate

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -8,11 +8,17 @@ class TeachersController < ApplicationController
   
   post '/login' do
     #Find the user
-    @teacher = Teacher.find_by(username: params[:username])
+    @teacher = Teachers.find_by(username: params[:username])
     #Authenticate the user - verify the user is who they say they are 
-    #log the user in
+    if @teacher.authenticate(params[:password])
+      
+    #log the user in = create the user session 
     #redirect to the user's landing page
     
+    else 
+      #tell the user they entered invalid credentials
+      #redirect them to the login page 
+    end 
   end
   #the purpose of this route is to receive the login form and sign the user in (create a session)
   
