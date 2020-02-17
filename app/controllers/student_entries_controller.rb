9d315201e98e40e6ec29923715f3c9da904a075b
Micah Levine <micah.levine@outlook.com>
2020-02-17 03:20:37 +0000

    changed student entry controller

--- app/controllers/student_entries_controller.rb
+++ app/controllers/student_entries_controller.rb
@@ -1,31 +1,49 @@
 class StudentEntriesController < ApplicationController
   #get student_entries/new to render a form to create new entry
+  get '/student_entries' do
+    @students = Student.all
+    erb :'/student_entries/index'
+  end
+  
   get '/student_entries/new' do
     erb :'/student_entries/new'
   end 
-  #post student_entries to create new student entry, post should end in redirect, does not need a rendered page 
+  
+   
   post '/student_entries' do
-    raise params.inspect
-    if !logged_in?
-      redirect '/'
+      @student = Student.create(params[:student])
+    if !params["name"]["grade_level"].empty?
+      @student.user = User.create(name: params["name"]["grade_level"])
     end
     
-    if params[:content] != ""
-      @student_entry = StudentEntry.create(content: params[:content], user_id: current_user.id)
-      redirect '/student_entries/#{@student_entry.id)'
-    else 
-      redirect '/student_entries/new'
-    end 
+    @student.save
+    
+    redirect to "students/#{@student.id}"
   end
-end 
   
-  #show page for student student_entries - don't think I need this
+  get '/student_entries/id' do
+    @student = Student.find(params[:id])
+    erb :'/student_entries/show'
+  end
   
-  #get '/student_entries/:id' do
-   # @student_entry = StudentEntry.find(params[:id])
-    #erb :
+  get '/student_entries/id/edit' do
+    @student = Student.find(params[:id])
+    erb :'/student_entries/edit'
+  end
   
-  
-  #index route for all student_entries
+  patch '/student_entries/:id' do
+    @student = Student.find(params[:id])
+    @student.update(params[:student])
+    
+    if !params["name"]["grade_level"].empty?
+      @student.user = User.create(name: params["student"]["grade_level"])
+    end
+    
+    @student.save
+    
+    redirect to "students/#{@student.id}"
+  end
+end
+
   
 
